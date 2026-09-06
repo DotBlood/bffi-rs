9f39ca1d480be280742c4705b721ccb2e3e19955
z2net <dev@z2net.com>
2026-09-06 14:02:01 +0300

    feat(callback): add CallbackError with BffiError conversion

--- crates/bffi-callback/src/lib.rs
+++ crates/bffi-callback/src/lib.rs
@@ -23,6 +23,8 @@
 
 #![cfg_attr(test, allow(clippy::expect_used, clippy::panic, clippy::unwrap_used))]
 
+pub mod error;
 pub mod value;
 
+pub use error::CallbackError;
 pub use value::{CallbackSig, Value, ValueType};
